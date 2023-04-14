93dc2d4981f0bbab6c6c3947eb0f13c875f3a90a
maxymlyskov <raypember02@gmail.com>
2023-04-14 11:52:30 +0300

    Added loading/ submit state to the form

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,6 @@
-import { PageLayout, Input, PasswordInput } from "components/common";
+import { PageLayout, Input, PasswordInput, Button } from "components/common";
 import React, { useState } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 const Form = styled.form`
@@ -11,10 +12,18 @@ const Form = styled.form`
   box-sizing: border-box;
   color: black;
   border-radius: 4px;
+
+  .alt-text {
+    text-align: center;
+    margin: 10px 0;
+  }
 `;
+
+let timeout;
 
 function Login() {
   const [formFields, setFormFields] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (e) => {
     e.persist();
@@ -24,10 +33,27 @@ function Login() {
       [e.target.name]: e.target.value,
     }));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, []);
+
   return (
     <PageLayout>
       <h1>Login</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Input
           value={formFields.username}
           onChange={handleInputChange}
@@ -39,6 +65,17 @@ function Login() {
           onChange={handleInputChange}
           name="password"
         />
+        <Button large type="sumbit" disabled={loading}>
+          {loading ? "Loading..." : "Login"}
+        </Button>
+        {!loading && (
+          <>
+            <div className="alt-text">or</div>
+            <Button secondary type="button">
+              Register
+            </Button>
+          </>
+        )}
       </Form>
     </PageLayout>
   );

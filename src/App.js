f7a1c787fc0c2e20e3c7befc04274c4bdb2c0df2
maxymlyskov <raypember02@gmail.com>
2023-04-14 09:27:16 +0300

    Added global styles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { Button } from "components/common";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script';
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <h1>Styled components</h1>
-      <Button>Test</Button>
-    </div>
+    <>
+      <GlobalStyle />
+      <div>
+        <h1>Styled components</h1>
+        <Button>Test</Button>
+      </div>
+    </>
   );
 }
 

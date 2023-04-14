6014974854cbd24a69e0d518586e654999322c45
maxymlyskov <raypember02@gmail.com>
2023-04-14 13:26:19 +0300

    Added proptypes usage.

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
 
 const largeStyles = ({ large }) => {
   if (large) {
@@ -33,5 +34,10 @@ const Button = styled.button`
     color: #666;
   }
 `;
+
+Button.propTypes = {
+  large: PropTypes.bool,
+  secondary: PropTypes.bool,
+};
 
 export { Button };

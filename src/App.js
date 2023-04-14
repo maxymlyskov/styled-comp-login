b8e4927b7ccb524cb5b06e43446449c60107cc01
maxymlyskov <raypember02@gmail.com>
2023-04-14 09:21:52 +0300

    Init commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "components/common";
+
+function App() {
+  return (
+    <div>
+      <h1>Styled components</h1>
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
